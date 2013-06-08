440ae102460a99bdbeb562a32279c52b3eb67edb
Charlie Hadden <charlie@hadden.me>
2013-06-08 16:41:31 +0100

    Adds vimconf as submodule

--- install.sh
+++ install.sh
@@ -10,10 +10,14 @@ echo "Moving old dotfiles to backup directory."
 mv ~/.bash_profile $backup
 mv ~/.bashrc $backup
 mv ~/.vimrc $backup
+mv ~/.vimrc.first $backup
+mv ~/.vimrc.bundles $backup
+mv ~/.vimrc.last $backup
+mv ~/.vim/ $backup
 mv ~/.xinitrc $backup
 
 echo "Creating symlinks to new files."
 ln -s $dir/bash_profile ~/.bash_profile
 ln -s $dir/bashrc ~/.bashrc
-ln -s $dir/vimrc ~/.vimrc
+ln -s $dir/vimconf/.vimrc ~/.vimrc
 ln -s $dir/xinitrc ~/.xinitrc
